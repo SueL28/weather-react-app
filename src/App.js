a9c5c4028103a5ec42dc2bf337be970e187217c5
SueL28 <69327405+SueL28@users.noreply.github.com>
2021-08-27 23:35:38 -0400

    Updated orange colour in css for type and buttons

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
 
 
         <div className="app-container">
-          <h1 className="weather-title">Weather Search <AnimatedEmoji icon="CLEAR_DAY" colour="white" size={70} /></h1>
+          <h1 className="weather-title">Weather Search <AnimatedEmoji icon="CLEAR_DAY" colour="#f1b648" size={70} /></h1>
           <span>
             <WeatherApp />
           </span>
           <div>
             <p className="footer-links">
-              <a href="https://github.com/SueL28/weather-react-app">Open-source code</a> by <a href="https://www.linkedin.com/in/su-anne-lieu/">Su-Anne Lieu</a>
+              <a href="https://github.com/SueL28/weather-react-app" target="_blank">Open-source code</a> by <a href="https://www.linkedin.com/in/su-anne-lieu/" target="_blank">Su-Anne Lieu</a>
             </p>
           </div>
         </div>
